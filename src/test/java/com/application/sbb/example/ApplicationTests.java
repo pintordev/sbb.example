2b527cfa363197235c883713da9b833b66995040
pintordev <pintor.dev@gmail.com>
2023-05-03 08:56:35 +0900

    jump to springboot to 2.14

--- src/test/java/com/application/sbb/example/ApplicationTests.java
+++ src/test/java/com/application/sbb/example/ApplicationTests.java
@@ -9,10 +9,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
@@ -23,17 +23,17 @@ class ApplicationTests {
 	@Autowired
 	private AnswerRepository answerRepository;
 
-	@Transactional
 	@Test
 	void testJPA() {
 		Optional<Question> oq = this.questionRepository.findById(2);
 		assertTrue(oq.isPresent());
 		Question q = oq.get();
 
-		List<Answer> answerList = q.getAnswerList();
-
-		assertEquals(1, answerList.size());
-		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+		Answer a = new Answer();
+		a.setContent("네 자동으로 생성됩니다.");
+		a.setQuestion(q);
+		a.setCreateDate(LocalDateTime.now());
+		this.answerRepository.save(a);
 	}
 
 }
